dfa610a5b3527a8d41b957578ea21d1f3a025d59
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-04-01 13:13:37 -0400

    fix: cargo
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@
 // You should have received a copy of the GNU General Public License along with
 // this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::{env, fs, io, process::Command, thread, time::Duration};
+use std::{env, fs, io, process::Command, thread};
 
 use crate::{
     app::App,
