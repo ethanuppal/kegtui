ef4da9b97fc5f0e2e367bd9c4acd346f3ffd23ee
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-07-17 19:04:13 -0700

    fix: Correct Cargo.toml
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@
 use std::path::PathBuf;
 
 use iced::{
-    Font, Length, Size, Subscription, Task, Theme,
     advanced::graphics::core::Element, font::Family, widget::container, window,
+    Font, Length, Size, Subscription, Task, Theme,
 };
-use iced_term::TerminalView;
+use iced_term::{ColorPalette, TerminalView};
 
 fn main() -> iced::Result {
     iced::application(App::title, App::update, App::view)
@@ -57,6 +57,37 @@ impl App {
         let mut executable_path = resources_root().unwrap_or_default();
         executable_path.push(TUI_EXECUTABLE);
 
+        let oxocarbon = ColorPalette {
+            foreground: String::from("#e0def4"),
+            background: String::from("#191724"),
+            black: String::from("#26233a"),
+            red: String::from("#eb6f92"),
+            green: String::from("#31748f"),
+            yellow: String::from("#f6c177"),
+            blue: String::from("#9ccfd8"),
+            magenta: String::from("#c4a7e7"),
+            cyan: String::from("#ebbcba"),
+            white: String::from("#e0def4"),
+            bright_black: String::from("#6e6a86"),
+            bright_red: String::from("#eb6f92"),
+            bright_green: String::from("#31748f"),
+            bright_yellow: String::from("#f6c177"),
+            bright_blue: String::from("#9ccfd8"),
+            bright_magenta: String::from("#c4a7e7"),
+            bright_cyan: String::from("#ebbcba"),
+            bright_white: String::from("#e0def4"),
+            bright_foreground: None,
+            dim_foreground: String::from("#6e6a86"),
+            dim_black: String::from("#191724"),
+            dim_red: String::from("#b85879"),
+            dim_green: String::from("#285a72"),
+            dim_yellow: String::from("#c49a5f"),
+            dim_blue: String::from("#7fa3ad"),
+            dim_magenta: String::from("#9d85b8"),
+            dim_cyan: String::from("#bc9596"),
+            dim_white: String::from("#b4b1c7"),
+        };
+
         let term_id = 0;
         let term_settings = iced_term::settings::Settings {
             font: iced_term::settings::FontSettings {
@@ -67,7 +98,9 @@ impl App {
                 },
                 ..Default::default()
             },
-            theme: iced_term::settings::ThemeSettings::default(),
+            theme: iced_term::settings::ThemeSettings {
+                color_pallete: Box::new(oxocarbon),
+            },
             backend: iced_term::settings::BackendSettings {
                 shell: executable_path.to_string_lossy().to_string(),
                 ..Default::default()
