580c9c6b5d95e6307895932b6ade222eca58a84a
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2025-07-17 19:19:02 -0700

    fix: Oxocarbon and styling
    Signed-off-by: Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@
 use std::path::PathBuf;
 
 use iced::{
-    advanced::graphics::core::Element, font::Family, widget::container, window,
     Font, Length, Size, Subscription, Task, Theme,
+    advanced::graphics::core::Element,
+    alignment::Horizontal,
+    font::Family,
+    widget::{column, container, text},
+    window,
 };
 use iced_term::{ColorPalette, TerminalView};
 
@@ -19,6 +23,7 @@ fn main() -> iced::Result {
         .font(
             include_bytes!("../fonts/HackNerdFontMono-Regular.ttf").as_slice(),
         )
+        .default_font(Font::with_name("Hack Nerd Font Mono"))
         .run_with(App::new)
 }
 
@@ -58,40 +63,40 @@ impl App {
         executable_path.push(TUI_EXECUTABLE);
 
         let oxocarbon = ColorPalette {
-            foreground: String::from("#e0def4"),
-            background: String::from("#191724"),
-            black: String::from("#26233a"),
-            red: String::from("#eb6f92"),
-            green: String::from("#31748f"),
-            yellow: String::from("#f6c177"),
-            blue: String::from("#9ccfd8"),
-            magenta: String::from("#c4a7e7"),
-            cyan: String::from("#ebbcba"),
-            white: String::from("#e0def4"),
-            bright_black: String::from("#6e6a86"),
-            bright_red: String::from("#eb6f92"),
-            bright_green: String::from("#31748f"),
-            bright_yellow: String::from("#f6c177"),
-            bright_blue: String::from("#9ccfd8"),
-            bright_magenta: String::from("#c4a7e7"),
-            bright_cyan: String::from("#ebbcba"),
-            bright_white: String::from("#e0def4"),
+            foreground: String::from("#dde1e6"),
+            background: String::from("#161616"),
+            black: String::from("#262626"),
+            red: String::from("#ff7eb6"),
+            green: String::from("#42be65"),
+            yellow: String::from("#82cfff"),
+            blue: String::from("#33b1ff"),
+            magenta: String::from("#ee5396"),
+            cyan: String::from("#3ddbd9"),
+            white: String::from("#dde1e6"),
+            bright_black: String::from("#393939"),
+            bright_red: String::from("#ff7eb6"),
+            bright_green: String::from("#42be65"),
+            bright_yellow: String::from("#82cfff"),
+            bright_blue: String::from("#33b1ff"),
+            bright_magenta: String::from("#ee5396"),
+            bright_cyan: String::from("#3ddbd9"),
+            bright_white: String::from("#ffffff"),
             bright_foreground: None,
-            dim_foreground: String::from("#6e6a86"),
-            dim_black: String::from("#191724"),
-            dim_red: String::from("#b85879"),
-            dim_green: String::from("#285a72"),
-            dim_yellow: String::from("#c49a5f"),
-            dim_blue: String::from("#7fa3ad"),
-            dim_magenta: String::from("#9d85b8"),
-            dim_cyan: String::from("#bc9596"),
-            dim_white: String::from("#b4b1c7"),
+            dim_foreground: String::from("#525252"),
+            dim_black: String::from("#161616"),
+            dim_red: String::from("#cc6591"),
+            dim_green: String::from("#359851"),
+            dim_yellow: String::from("#69a7cc"),
+            dim_blue: String::from("#2990cc"),
+            dim_magenta: String::from("#be4378"),
+            dim_cyan: String::from("#31b1ae"),
+            dim_white: String::from("#b4b7ba"),
         };
 
         let term_id = 0;
         let term_settings = iced_term::settings::Settings {
             font: iced_term::settings::FontSettings {
-                size: 18.0,
+                size: 24.0,
                 font_type: Font {
                     family: Family::Name("Hack Nerd Font Mono"),
                     ..Default::default()
@@ -145,9 +150,19 @@ impl App {
     }
 
     fn view(&self) -> Element<Event, Theme, iced::Renderer> {
-        container(TerminalView::show(&self.term).map(Event::Terminal))
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .into()
+        container(
+        column![
+            container(
+                text("Note: this has been ported directly from a terminal app into a GUI app, so there may be some issues. Remember to click on the app to focus it if needed.")
+                    .width(Length::Fill)
+                    .align_x(Horizontal::Left)
+            )
+            .padding(4),
+            TerminalView::show(&self.term).map(Event::Terminal)
+        ]
+    )
+    .width(Length::Fill)
+    .height(Length::Fill)
+    .into()
     }
 }
